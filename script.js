f9927a17079f94d1fb350eb95bbd15c75ad6d6d3
Marco-Petronella <marco.petronella85@gmail.com>
2026-02-28 12:45:49 +0100

    commenti per lo svolgimento

--- script.js
+++ script.js
@@ -12,6 +12,14 @@ scriviamo sempre prima dei commenti in italiano per capire cosa vogliamo fare
 proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano" */
 
 /*
+ ci serve un contatore per ciclo che scorra i numeri da 1 a 100
+ durante l'esecuzione del ciclo controlliamo se la divisione per 3 del numero corrente ci dà resto 0, in tal caso è divisibile 
+ quindi stiampiamo "Fizz"
+ eseguiamo  un controllo simile che verifichi la divisione per 5 e in quel caso stampiamo Buzz
 
+ dal momento che vogliamo che se il numero è divisibile per entrambi stampi "FizzBuzz", creerei una variabile globale per il messaggio
+ la svuoterei all'inizio del ciclo, poi se è divisibile per 3 ci metterei dentro Fizz, se è divisibile per 5 ci inserirei Buzz, in tal modo
+ se è divisibile per entrambi la variabile conterrà entrambi i valori, poi prima di chiudere il ciclo la stamperei.
 */
+
 
